38b9fa83a22ef57327fe4195a779570396996ab5
herman <herman.anez.dev@gmail.com>
2026-04-23 16:33:22 +0200

    .

--- A/Herramientas/hygene/Ejemplos/1/_templates/ddd/entity/prompt.js
+++
@@ -1,8 +0,0 @@
-module.exports = [
-  {
-    type: 'input',
-    name: 'name',
-    message: "¿Nombre de la entidad? (ej. User)",
-    validate: (val) => val.length > 0 || "¡El nombre es obligatorio!"
-  }
-];
--- A/Herramientas/hygene/Ejemplos/generator/entity-config.js
+++
@@ -1,7 +0,0 @@
-// entity-config.js template
-module.exports = {
-  moduleName: "HumanResources",
-  name: "Employee",
-  targetPath: "src",
-  addUseCases: true
-};
